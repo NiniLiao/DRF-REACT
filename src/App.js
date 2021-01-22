48759b6975bfc20fddd616642fa300ddd7df5279
Nini Liao <u9513031@cmu.edu.tw>
2021-01-22 16:44:05 +0800

    modified https

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class connectionExample extends React.Component {
     componentDidMount() {
-        const apiUrl = 'http://127.0.0.1:8000/api/';
+        const apiUrl = 'https://127.0.0.1:8000/api/';
         fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => console.log(data));
